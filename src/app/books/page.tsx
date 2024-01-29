e0c7b7760d10f0dfc3d982fa24fb5f1bef34aed5
spencer-rafada <spencer.byui@gmail.com>
2024-01-28 20:56:02 -0700

    added initial barebones for fetching books using searchParams

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,12 +1,30 @@
 import { Metadata } from 'next'
 import React from 'react'
-import { useSearchProvider } from '@/app/providers/SearchProvider'
 
 export const metadata: Metadata = {
   title: 'VITLIB | Books',
   description: 'Access all information about a book here',
 }
 
-export default function BooksPage() {
-  return <div>BooksPage</div>
+async function getBooks(query: string) {
+  const res = await fetch(
+    `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}`
+  )
+  const books = await res.json()
+
+  return books
+}
+
+export default async function BooksPage({
+  searchParams,
+}: {
+  searchParams: { q: string }
+}) {
+  const books = await getBooks(searchParams.q)
+  return (
+    <>
+      <div>{searchParams.q}</div>
+      <div>{books.docs[0].title}</div>
+    </>
+  )
 }
