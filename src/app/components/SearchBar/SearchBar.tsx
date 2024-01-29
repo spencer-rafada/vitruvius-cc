2690a52c7abc3959ba396227a1814bfdadcbefcf
spencer-rafada <spencer.byui@gmail.com>
2024-01-29 01:50:43 -0700

    refactored search bar to be wrapped inside a form

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -3,38 +3,45 @@ import { useSearchProvider } from '@/app/providers/SearchProvider'
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import { TextField } from '@radix-ui/themes'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SearchBar() {
-  const { search, setSearch } = useSearchProvider()
+  const { search, setSearch, getBooks, setLoading } = useSearchProvider()
   const router = useRouter()
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
   }
 
-  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      router.push('/books')
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    router.push('/books')
+    setLoading(true)
+    try {
+      await getBooks()
+    } catch (error) {
+      console.log(error)
     }
+    setLoading(false)
   }
 
   return (
-    <TextField.Root
-      data-testid='search-bar-container'
-      className='w-10/12 md:w-8/12'
-      size='3'
-      radius='full'
-    >
-      <TextField.Slot>
-        <MagnifyingGlassIcon height='18' width='18' />
-      </TextField.Slot>
-      <TextField.Input
-        placeholder='Search a book...'
-        value={search}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-      />
-    </TextField.Root>
+    <form className='w-10/12 md:w-8/12' onSubmit={onSubmit}>
+      <TextField.Root
+        data-testid='search-bar-container'
+        className='w-full'
+        size='3'
+        radius='full'
+      >
+        <TextField.Slot>
+          <MagnifyingGlassIcon height='18' width='18' />
+        </TextField.Slot>
+        <TextField.Input
+          placeholder='Search a book...'
+          value={search}
+          onChange={onChange}
+        />
+      </TextField.Root>
+    </form>
   )
 }
