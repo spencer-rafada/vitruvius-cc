d3b2b70753a56d3611185cd7dd75305a37581d88
spencer-rafada <spencer.byui@gmail.com>
2024-01-29 17:56:17 -0700

    added conditional rendering for each components on the BooksContent

--- src/app/books/components/BooksContent.tsx
+++ src/app/books/components/BooksContent.tsx
@@ -3,20 +3,23 @@ import { useSearchProvider } from '@/app/providers/SearchProvider'
 import React from 'react'
 import Loading from './Loading'
 import { Box, Text } from '@radix-ui/themes'
+import BooksList from './BooksList'
 
 export default function BooksContent() {
-  const { lastSearch, loading } = useSearchProvider()
+  const { lastSearch, loading, searchResult } = useSearchProvider()
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <Box data-testid='books-header-container'>
-          <Text size='4'>
-            You searched for: <Text weight='bold'>{lastSearch}</Text>
-          </Text>
-        </Box>
-      )}
+      {loading && <Loading />}
+      <>
+        {!loading && lastSearch && (
+          <Box data-testid='books-header-container'>
+            <Text size='4'>
+              You searched for: <Text weight='bold'>{lastSearch}</Text>
+            </Text>
+          </Box>
+        )}
+        {!loading && searchResult && <BooksList books={searchResult} />}
+      </>
     </>
   )
 }
