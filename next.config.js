6c965d639f07bd4ad0e2dbdca17b53774e60177a
spencer-rafada <spencer.byui@gmail.com>
2024-01-30 23:09:34 -0700

    config next config depending on node env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',
-  images: {
-    unoptimized: true,
-  },
+let nextConfig
+
+if (process.env.NODE_ENV === 'production') {
+  nextConfig = {
+    // configuration for production
+    images: {
+      unoptimized: false,
+    },
+  }
+} else {
+  nextConfig = {
+    // configuration for development
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  }
 }
 
 module.exports = nextConfig
