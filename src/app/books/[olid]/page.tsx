304c405be2c889b8ce971f9156498f6b6d8e7030
spencer-rafada <spencer.byui@gmail.com>
2024-01-30 21:57:29 -0700

    generated static params for pagination for testing

--- src/app/books/[olid]/page.tsx
+++ src/app/books/[olid]/page.tsx
@@ -1,20 +1,20 @@
-export const dynamic = 'force-dynamic'
-
 import React from 'react'
 
 type Props = {
   params: { olid: string }
 }
 
-// export async function generateStaticParams() {
-//   const books = await fetch(
-//     'https://openlibrary.org/search.json?q=the+hunger+games&fields=key,title,author_name,editions,isbn,publish_date,ratings_average,ratings_count,publisher,author_key,language,first_sentence,person,place,subject'
-//   ).then((res) => res.json())
+// This function is called during the build (build time) for GitHub Actions
+export async function generateStaticParams() {
+  const books = await fetch(
+    'https://openlibrary.org/search.json?q=the+hunger+games&fields=key,title,author_name,editions,isbn,publish_date,ratings_average,ratings_count,publisher,author_key,language,first_sentence,person,place,subject'
+  ).then((res) => res.json())
+  const book = books.docs.map((book: any) => ({
+    olid: book.editions?.docs?.[0]?.key?.replace('/books/', ''),
+  }))
 
-//   return books.docs.map((book: any) => ({
-//     params: { olid: book.key.replace('/works/', '') },
-//   }))
-// }
+  return book
+}
 
 export default function DetailsPage({ params }: Props) {
   return <div>{params.olid}</div>
