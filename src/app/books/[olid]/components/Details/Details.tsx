eb4875a4d9dc279ac60c6cd0826e0df56b7b4e5d
spencer-rafada <spencer.byui@gmail.com>
2024-02-01 01:25:42 -0700

    refactored details tab to pass unit tests

--- src/app/books/[olid]/components/Details/Details.tsx
+++ src/app/books/[olid]/components/Details/Details.tsx
@@ -5,24 +5,34 @@ import React from 'react'
 
 export default function Details({ bookId }: { bookId: string }) {
   const { book, work, loading, error } = useBookDetails({ olid: bookId })
-  console.log('book', book)
-  console.log('work', work)
 
   return (
     <>
       {!loading && !error && book && work && (
-        <Flex direction='column' gap={{ initial: '3', md: '4' }}>
+        <Flex
+          direction='column'
+          gap={{ initial: '3', md: '4' }}
+          data-testid='books-details-details-tab'
+        >
           <Heading size={{ initial: '6', md: '8' }} as='h2'>
             Book Details
           </Heading>
           <Heading size={{ initial: '5', md: '7' }} as='h3'>
             ID Numbers
           </Heading>
-          <Flex direction='row' gap={{ initial: '3', md: '4' }}>
+          <Flex
+            direction='row'
+            gap={{ initial: '3', md: '4' }}
+            data-testid='books-details-openlibrary-id'
+          >
             <Text weight='medium'>OpenLibrary ID</Text>
             <Text>{book?.key.replace('/books/', '')}</Text>
           </Flex>
-          <Flex direction='row' gap={{ initial: '3', md: '4' }}>
+          <Flex
+            direction='row'
+            gap={{ initial: '3', md: '4' }}
+            data-testid='books-details-isbn'
+          >
             <Text weight='medium'>ISBN</Text>
             <Text>{book?.isbn?.[0] || book?.isbn?.[1]}</Text>
           </Flex>
--- src/app/books/[olid]/components/Details/__tests__/Details.test.tsx
+++ src/app/books/[olid]/components/Details/__tests__/Details.test.tsx
@@ -1,7 +1,28 @@
 import { screen, render } from '@testing-library/react'
 import Details from '../Details'
+import useBookDetails from '@/app/hooks/useBookDetails'
+
+jest.mock('../../../../../hooks/useBookDetails.tsx')
 
 describe('Details', () => {
+  beforeEach(() => {
+    ;(useBookDetails as jest.Mock).mockReturnValue({
+      work: { key: '/books/OL37079411M', id_amazon: ['123'] },
+      book: {
+        isbn: ['9781408855652'],
+        key: '/books/OL37079411M',
+        language: ['eng'],
+        publish_date: ['2014'],
+        publisher: ['Bloomsbury Childrens'],
+        title: "Harry Potter and the Philosopher's Stone",
+      },
+      error: false,
+      loading: false,
+    })
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
   it('should render the container', () => {
     render(<Details bookId='OL37079411M' />)
     expect(screen.getByTestId('books-details-details-tab')).toBeInTheDocument()
